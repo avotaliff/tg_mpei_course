3b2b1bda9ea4cc1d43bf518cf71e39ddd6a36355
Ekaterina Filatova <54913797+avotaliff@users.noreply.github.com>
2019-12-03 20:45:23 +0300

    Create 13.133. Clone Graph.cpp

---
+++ 13.133. Clone Graph.cpp
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/clone-graph/
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+//DFS
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) return NULL;
+        if (copies.find(node) == copies.end()){
+            copies[node] = new Node (node->val, {});
+            for (Node* neighbors : node -> neighbors){
+                copies[node] -> neighbors.push_back(cloneGraph(neighbors));
+            }
+        }
+        return copies[node];
+    }
+    private:
+        unordered_map<Node*, Node*> copies;
+};
