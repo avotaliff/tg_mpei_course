777989b5af7d8537d083e03d394ecdec6463eca7
Ekaterina Filatova <54913797+avotaliff@users.noreply.github.com>
2019-12-06 23:05:10 +0300

    Update 13.127. Word Ladder.cpp

--- 13.127. Word Ladder.cpp
+++ 13.127. Word Ladder.cpp
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/word-ladder/submissions/
+//https://leetcode.com/problems/word-ladder/
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
